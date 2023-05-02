e1c684296d3b51abe6fdc2c52b624560e9c26a06
cqxg <ihar.o.ivaniuk@gmail.com>
2023-05-02 19:02:08 +0200

    feat: some Counting String Subsequences rework

--- 4-kyu/counting-string-subsequences.js
+++ 4-kyu/counting-string-subsequences.js
@@ -1,18 +1,13 @@
 const countSubsequences = (needle, haystack) => {
   const MOD = 100000000;
-  const dp = Array.from({ length: haystack.length + 1 }, () =>
-    Array.from({ length: needle.length + 1 }, () => 0)
-  );
-  for (let i = 0; i <= haystack.length; i++) {
-    dp[i][0] = 1;
-  }
-  for (let i = 1; i <= haystack.length; i++) {
-    for (let j = 1; j <= needle.length; j++) {
-      dp[i][j] = dp[i - 1][j];
-      if (haystack[i - 1] === needle[j - 1]) {
-        dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+  const dp = Array.from({ length: needle.length + 1 }, () => 0);
+  dp[0] = 1;
+  for (let i = 0; i < haystack.length; i++) {
+    for (let j = needle.length; j > 0; j--) {
+      if (haystack[i] === needle[j - 1]) {
+        dp[j] = (dp[j] + dp[j - 1]) % MOD;
       }
     }
   }
-  return dp[haystack.length][needle.length] % MOD;
+  return dp[needle.length];
 };
